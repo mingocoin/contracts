b7c6d5eed5dab24a5ce4a46dd717d742a4e91daa
Ryan Leckey <leckey.ryan@gmail.com>
2017-09-19 13:13:58 -0700

    Track number of unique contributions

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -8,6 +8,10 @@ import './MultiOwnable.sol';
 contract Crowdsale is MultiOwnable {
     using SafeMath for uint256;
 
+    // Record contributors
+    mapping (address => bool) contributors;
+    uint256 contributorCount;
+
     // The token being sold
     MintableToken public token;
 
@@ -112,6 +116,16 @@ contract Crowdsale is MultiOwnable {
         );
 
         wallet.transfer(amount);
+
+        if (!contributors[beneficiary]) {
+          contributors[beneficiary] = true;
+          contributorCount += 1;
+        }
+    }
+
+    // Get number of contributors
+    function numberOfContributors() returns (uint256) {
+      return contributorCount;
     }
 
     // https://github.com/OpenZeppelin/zeppelin-solidity/blob/1737555b0dda2974a0cd3a46bdfc3fb9f5b561b9/contracts/crowdsale/FinalizableCrowdsale.sol#L23
